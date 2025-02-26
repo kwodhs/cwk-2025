f0ccaeee84d7387e18deb7668d4202722e7dabe6
Yevhenii Humennyi <5karatgg@gmail.com>
2025-02-26 23:39:56 +0200

    replaced compression format

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Humennyi Yevhenii-The Greatest Canyons in the World-0.1.0";
+const contentToCache = [
+    "Build/BUILD.loader.js",
+    "Build/BUILD.framework.js.gz",
+    "Build/BUILD.data.gz",
+    "Build/BUILD.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
