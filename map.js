8b06066899b9d7cd26c8edcb35b770f99846e858
Yevhenii Humennyi <5karatgg@gmail.com>
2025-03-14 12:42:10 +0200

    Implemented map to canyons info and added this feature in site features

---
+++ map.js
@@ -0,0 +1,27 @@
+function myMap() {
+    var grandProp= {
+        center:new google.maps.LatLng(36.0997795,-112.1228058),
+        zoom:5,
+    };
+    var glenProp= {
+        center:new google.maps.LatLng(36.8644483,-111.5982593),
+        zoom:5,
+    };
+    var sylakProp= {
+        center:new google.maps.LatLng(43.0101947,46.8157642),
+        zoom:5,
+    };
+    var colcaProp= {
+        center:new google.maps.LatLng(-15.6574411,-72.1797266),
+        zoom:5,
+    };
+    var uvacProp= {
+        center:new google.maps.LatLng(43.3564319,19.9577047),
+        zoom:5,
+    };
+    var grand = new google.maps.Map(document.getElementById("grandMap"),grandProp);
+    var glen = new google.maps.Map(document.getElementById("glenMap"),glenProp);
+    var sylak = new google.maps.Map(document.getElementById("sylakMap"),sylakProp);
+    var colca = new google.maps.Map(document.getElementById("colcaMap"),colcaProp);
+    var uvac = new google.maps.Map(document.getElementById("uvacMap"),uvacProp);
+}
