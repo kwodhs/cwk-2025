15a7459841601d07493ed1b30439e21f26690853
Yevhenii Humennyi <5karatgg@gmail.com>
2025-03-16 23:21:12 +0200

    fixed map scale

--- map.js
+++ map.js
@@ -1,23 +1,23 @@
 function myMap() {
     var grandProp= {
         center:new google.maps.LatLng(36.0997795,-112.1228058),
-        zoom:5,
+        zoom:10,
     };
     var glenProp= {
         center:new google.maps.LatLng(36.8644483,-111.5982593),
-        zoom:5,
+        zoom:10,
     };
     var sylakProp= {
         center:new google.maps.LatLng(43.0101947,46.8157642),
-        zoom:5,
+        zoom:10,
     };
     var colcaProp= {
         center:new google.maps.LatLng(-15.6574411,-72.1797266),
-        zoom:5,
+        zoom:10,
     };
     var uvacProp= {
         center:new google.maps.LatLng(43.3564319,19.9577047),
-        zoom:5,
+        zoom:10,
     };
     var grand = new google.maps.Map(document.getElementById("grandMap"),grandProp);
     var glen = new google.maps.Map(document.getElementById("glenMap"),glenProp);
